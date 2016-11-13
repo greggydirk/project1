0423092e6675db6d5bfdcbcb8d9619a0d4e3a3ef
greggy <greggydirkmaitimoe@gmail.com>
2016-11-13 14:20:42 +0700

    buat register

---
+++ project1/Controllers/RegistrasiController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using project1.DAL;
+using project1.Models;
+
+using System.Security.Cryptography;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace project1.Controllers
+{
+    public class RegistrasiController : Controller
+    {
+        // GET: Registrasi
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pengguna pengguna)
+        {
+            PenggunaDAL pengDAL = new PenggunaDAL();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    pengDAL.Refistrasi(pengguna);
+                    ViewBag.Pesan = "Data Pengguna berhasil di Tambah ";
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View();
+            }
+        }
+
+    }
+}
---
+++ project1/DAL/PenggunaDAL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Security.Cryptography;
+using project1.Models;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace project1.DAL
+{
+    public class PenggunaDAL
+    {
+
+        private string GetConnStr()
+        {
+            return @"Data Source = .\SQLEXPRESS;Initial Catalog = Sample;Integrated Security = True";
+        }
+
+        public string GetMD5Hash(string password)
+        {
+            UnicodeEncoding unicode = new UnicodeEncoding ();
+            byte[] datahash = unicode.GetBytes(password);
+            MD5 md5 = new MD5CryptoServiceProvider();
+            var hasil = md5.ComputeHash(datahash);
+            return Convert.ToBase64String(hasil);
+
+        }
+
+        public void Refistrasi(Pengguna pengguna)
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"insert into Pengguna(Username,Password,Aturan) values (@Username,@password,@Aturan)";
+                SqlCommand cmd = new SqlCommand(strSql, conn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("Username", pengguna.Username);
+                cmd.Parameters.AddWithValue("Password", GetMD5Hash(pengguna.Password));
+                cmd.Parameters.AddWithValue("Aturan", pengguna.Aturan);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    throw new Exception(ex.Number.ToString() + " - " + ex.Message);
+                }
+            }
+        }
+
+
+    }
+}
